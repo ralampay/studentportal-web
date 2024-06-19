5a6a5affa08f720d029327fb20e9c87bb5cafca2
ralampay <raphael.alampay@gmail.com>
2024-06-19 10:37:17 +0800

    mounted initial react application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("HEllo world!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(
+    <div>
+        Student Portal
+    </div>
+);
