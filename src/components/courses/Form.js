d27aa73a89c6d16fff562a29124a41780b1c96e2
ralampay <raphael.alampay@gmail.com>
2024-06-19 11:17:41 +0800

    added example of basic state management

--- src/components/courses/Form.js
+++ src/components/courses/Form.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default Form = (props) => {
+
+    // States of Form
+    const [name, setName] = useState("");
+
     return (
         <div>
             <h4>
@@ -12,9 +16,27 @@ export default Form = (props) => {
                     Name:
                 </label>
                 <input
+                    value={name}
                     className="form-control"
+                    onChange={(event) => {
+                        let val = event.target.value;
+                        setName(val);
+                    }}
                 />
             </div>
+            <hr/>
+            <table className="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <td>
+                            {name}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
