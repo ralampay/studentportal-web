8fdbde30225c0ccfec2fc8626e281340c17e0133
ralampay <raphael.alampay@gmail.com>
2024-08-14 09:31:33 +0800

    added ui for dashboard for exercise

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,6 +11,33 @@ export default Dashboard = () => {
         <div>
             Dashboard
             <hr/>
+            <div className="row">
+                <div className="col">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4>
+                                Number of Students
+                            </h4>
+                            <h6>
+                                0
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4>
+                                Number of Courses (fetch from backend)
+                            </h4>
+                            <h6>
+                                0
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr/>
             <Provider store={store}>
                 <Counter/>
                 <Controls/>
